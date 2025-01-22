833aab2fe75b21a60fc8330856eb6c8c09f4c519
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-01-21 16:37:16 -0800

    Redoing the project Clone and adding in Steamworks functionality(needs to be tested)

---
+++ GDIM_161_Horror_clone_0/Assets/_Scripts/Interactions/PickableItem.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using MessengerSystem;
+using TMPro;
+
+/// <summary>
+/// This class allows items to be picked up by the player.
+/// </summary>
+public class PickableItem : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Animator _textAnimation;
+    [SerializeField] private string _textName;
+    [SerializeField] private string _fadeIn;
+
+    private TextMeshProUGUI _textMesh;
+    private PlayerManager _playerManager;
+
+    void Start()
+    {
+        _playerManager = DataMessenger.GetGameObject(MessengerKeys.GameObjectKey.PlayerManager).GetComponent<PlayerManager>();
+        //Debug.Log($"{this.gameObject.name} found playerManager: {_playerManager}");
+        _textMesh = transform.GetComponentInChildren<TextMeshProUGUI>();
+        _textMesh.text = _textName;
+    }
+
+    public void Interact(int playerID)
+    {
+        Debug.Log($"Pickable Item {gameObject.name} recieved an Interact order from Player {playerID}");
+        _playerManager.GetPlayer(playerID).gameObject.GetComponent<HandInventory>().PickUpObject(transform.parent.gameObject);
+    }
+
+    public void Detected(int playerID)
+    {
+        Debug.Log($"Detected by player {playerID}, {_playerManager.GetPlayer(playerID).gameObject.ToString()}");
+        _textAnimation.SetBool(_fadeIn, true);
+    }
+
+    public void StoppedDetecting(int playerID)
+    {
+        _textAnimation.SetBool(_fadeIn, false);
+    }
+}
--- GDIM_161_Horror_clone_0/Assets/_Scripts/Others/PersistentObjects.cs
+++ GDIM_161_Horror_clone_0/Assets/_Scripts/Others/PersistentObjects.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    /// <summary>
+    /// This script sets the object it is attached to as Do not destroy in
+    /// scene load. If there is another object with the tag privided it will 
+    /// destroy itself as this object should be the only one with that tag.
+    /// </summary>
 public class PersistentObject : MonoBehaviour
 {
-    /*
-        This script sets the object it is attached to as Do not destroy in
-        scene load. If there is another object with the tag privided it will 
-        destroy itself as this object should be the only one with that tag.
-    */
-
     [SerializeField] private string _objectsTag;
 
     void Awake()
