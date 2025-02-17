099eedebda927eb69b8f03aab5ad7fd962d2a89b
malachek <malachykennedy1@gmail.com>
2025-02-17 06:17:44 -0800

    torch perlin

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
@@ -1,15 +1,58 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Interactions
 {
     public class Torch : PickableItem
     {
+        [Header("Gameplay Stuff")]
+        [SerializeField] [Range(1f, 20f)] float approxWoodLife_Minutes;
+        [SerializeField] [Range(0f, 1f)] float maxTickVariance_WoodBurn;
+        [SerializeField] [Range(1f, 1.2f)] float tickSpeedMultiplier_ByFloor;
+
+        [Header("Model Stuff")]
         [SerializeField] TorchModel model;
+        [SerializeField] Transform flameBase;
+
+        public float BurnTimer { get; private set; }
+        private float burnVelocity = 1f;
+        private float burnAcelleration = 0f;
         public bool Lit { get; private set; }
 
         protected override void Start()
         {
             base.Start();
+            BurnTimer = 60 * approxWoodLife_Minutes;
+            Lit = true;
+        }
+
+        private void Update()
+        {
+            if(Lit) Burn();
+            UpdateFlameOrientation();
+        }
+
+        private void Burn()
+        {
+            if (BurnTimer <= 0) BlowOutFlame();
+            BurnTimer -= burnVelocity * Time.deltaTime; // * tickSpeedMultiplier_ByFloor
+            burnVelocity = CalcSmoothRandom(burnVelocity);
+
+            Debug.Log($"burnAcceleration = {burnAcelleration}");
+            Debug.Log($"burnVelocity = {burnVelocity}");
+        }
+
+        private float CalcSmoothRandom(float x)
+        {
+            burnAcelleration = Random.Range(0f, 1f) * .1f - .05f;
+            x += burnAcelleration;
+            x = Mathf.Clamp(x, 1-maxTickVariance_WoodBurn, 1+maxTickVariance_WoodBurn);
+            return x;
+        }
+
+        private void UpdateFlameOrientation()
+        {
+            flameBase.eulerAngles = Vector3.up;
         }
 
         public override void UseItem(int playerId)
@@ -31,17 +74,8 @@ namespace Interactions
             Lit = false;
         }
 
-        public void BurnOutFlame() // via end of wood
-        {
-            if (!Lit) return;
-            Lit = false;
-        }
-
-        public void SmotherFlame() // via dropping
-        {
-            if (!Lit) return;
-            Lit = false;
-        }
+        //public void BurnOutFlame() // via end of wood
+        //public void SmotherFlame() // via dropping
 
         //watch velocity (and air pressure) or burn out
 
