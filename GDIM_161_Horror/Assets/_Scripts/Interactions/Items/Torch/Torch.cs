076b3a4e589cd292693fea9fec31c9519701499f
malachek <malachykennedy1@gmail.com>
2025-02-17 04:40:13 -0800

    torch start

---
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Interactions
+{
+    public class Torch : PickableItem
+    {
+        [SerializeField] TorchModel model;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
