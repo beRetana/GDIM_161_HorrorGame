48f8a7e786b6218e0facfbd4966160dd0bf1d767
malachek <malachykennedy1@gmail.com>
2025-02-18 03:22:53 -0800

    working on pyrolysis

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Codice.Client.Common.GameUI;
 
 namespace Interactions
 {
@@ -9,17 +10,25 @@ namespace Interactions
         [SerializeField, Range(1f, 20f)] float approxWoodLife_Minutes;
         [SerializeField, Range(0f, 1f)] float maxTickVariance_WoodBurn;
         [SerializeField, Range(1f, 1.2f)] float tickSpeedMultiplier_ByFloor;
+        [Tooltip("Pyrolysis is the process of thermal decomposition of materials at elevated temperatures, often in an inert atmosphere without access to oxygen.")]
+        [SerializeField, Range(1, 100)] int pyrolysisIncrements;
 
         [Header("Model Stuff")]
         [SerializeField] TorchModel model;
-        [SerializeField] Transform flameBase;
-        [SerializeField] Vector3 torchAnchor;
+        [SerializeField] Transform torchAnchor;
         [SerializeField] Transform torchWood;
 
+        [Header("Fire Stuff")]
+        [SerializeField] ParticleSystem flameVFX;
+        [SerializeField] Transform flameBase;
 
         public float BurnTimer { get; private set; }
         private float burnVelocity = 1f;
         private float burnAcelleration = 0f;
+
+        private float pyrolysisTimer;
+
+        private float maxTorchWoodScale;
         private float torchWoodScale;
         private float minTorchWoodScale;
         public bool Lit { get; private set; }
@@ -28,19 +37,45 @@ namespace Interactions
         {
             base.Start();
             BurnTimer = 60 * approxWoodLife_Minutes;
+            pyrolysisTimer = BurnTimer / pyrolysisIncrements;
             Lit = true;
+            maxTorchWoodScale = torchWood.localScale.y;
+            torchWoodScale = maxTorchWoodScale;
         }
 
         private void Update()
         {
-            if(Lit) Burn();
-            UpdateFlameOrientation();
+            if (!Lit) return;
+            UpdateFlameOrientation(); 
+        }
+
+        private void FixedUpdate()
+        {
+            if (!Lit) return;
+            Burn();
+            UpdatePyrolysis();
+        }
+
+        private void UpdatePyrolysis()
+        {
+            pyrolysisTimer -= burnVelocity * Time.time;
+            if (pyrolysisTimer <= 0)
+            {
+                WoodPyrolysis();
+                pyrolysisTimer = BurnTimer / pyrolysisIncrements;
+            }
+        }
+
+        private void WoodPyrolysis()
+        {
+            torchWoodScale = torchWoodScale * BurnTimer / 60 / approxWoodLife_Minutes;
+            torchWood.localScale = new Vector3(torchWood.localScale.x, torchWoodScale, torchWood.localScale.y);
         }
 
         private void Burn()
         {
             if (BurnTimer <= 0) BlowOutFlame();
-            BurnTimer -= burnVelocity * Time.deltaTime; // * tickSpeedMultiplier_ByFloor
+            BurnTimer -= burnVelocity * Time.time; // * tickSpeedMultiplier_ByFloor
             burnVelocity = CalcSmoothRandom(burnVelocity);
 
             Debug.Log($"burnAcceleration = {burnAcelleration}");
@@ -68,15 +103,22 @@ namespace Interactions
             //  point torch
         }
 
-        private void TurnOnFlame(bool setOn)
+        private void ToggleFlame(bool setOn)
         {
-            model.TurnOnFlame(setOn);
+            Lit = setOn;
+            flameVFX.gameObject.SetActive(setOn);
         }
 
         public void BlowOutFlame() // via wind
         {
             if (!Lit) return;
-            Lit = false;
+            ToggleFlame(false);
+        }
+
+        public void LightFlame()
+        {
+            if (Lit) return;
+            ToggleFlame(true);
         }
 
         //public void BurnOutFlame() // via end of wood
