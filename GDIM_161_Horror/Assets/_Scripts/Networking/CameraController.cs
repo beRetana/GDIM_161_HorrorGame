a4f41f4b4143219ad321c60ea83ba24463a2a163
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-02-25 22:20:23 -0800

    camera test #1

---
+++ GDIM_161_Horror/Assets/_Scripts/Networking/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Mirror;
+
+public class CameraController : NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject Cameraholder;
+    public Vector3 offset;
+
+    
+
+    public override void OnStartAuthority()
+    {
+        Cameraholder.SetActive(true);
+    }
+
+
+    public void Update()
+    {
+
+        if(SceneManager.GetActiveScene().name == "Game")
+        {
+
+            Cameraholder.transform.position = transform.position + offset;
+        
+         }
+    }
+
+}
