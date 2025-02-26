83119898e32049a5c9829e8edd3dd559b9c1304f
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-02-25 22:55:52 -0800

    cinebrain fix 2

--- GDIM_161_Horror/Assets/_Scripts/Networking/CameraController.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/CameraController.cs
@@ -9,12 +9,13 @@ public class CameraController : NetworkBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject Cameraholder;
     public Vector3 offset;
-
+    public GameObject Cinebrain;
     
 
     public override void OnStartAuthority()
     {
         Cameraholder.SetActive(true);
+        Cinebrain.SetActive(true);
     }
 
 
