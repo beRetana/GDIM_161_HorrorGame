daf9469bd47f0adaab546a5a5b41365b218c1ed2
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-02-26 23:19:12 -0800

    temp stuff lobby yes

--- GDIM_161_Horror/Assets/_Scripts/Networking/PlayerMovementController_test.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/PlayerMovementController_test.cs
@@ -5,45 +5,39 @@ using UnityEngine.SceneManagement;
 public class PlayerMovementController_test : NetworkBehaviour
 {
     public float Speed = 0.1f;
-    //public GameObject PlayerModel;    - Removed for applying to main prefab
+    public GameObject PlayerModel; 
 
-    // A flag to ensure we only invoke once.
     
-    //private bool positionInvoked = false;
+   
 
-    // private void Start()
-    // {
-    //     PlayerModel.SetActive(false);
-    // }
+    private void Start()
+    {
+        PlayerModel.SetActive(false);
+    }
 
-    // private void Update()
-    // {
-    //     if (SceneManager.GetActiveScene().name == "Game")
-    //     {
-    //         if (!PlayerModel.activeSelf && !positionInvoked)
-    //         {
-    //             positionInvoked = true;
-    //             // Invoke the ActivatePlayer method after .5 seconds
-    //             Invoke("ActivatePlayer", .5f);
-    //         }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            if (!PlayerModel.activeSelf == false)
+            {
+                SetPosition();
+                PlayerModel.SetActive(true);
+            }
 
-    //         if (isOwned)
-    //         {
-    //             Movement();
-    //         }
-    //     }
-    // }
+            if (isOwned)
+            {
+                Movement();
+            }
+        }
+    }
 
-    // private void ActivatePlayer()
-    // {
-    //     SetPosition();
-    //     PlayerModel.SetActive(true);
-    // }
+   
 
-    // private void SetPosition()
-    // {
-    //     transform.position = new Vector3(Random.Range(-5, 5), 0.8f, Random.Range(7, 15));
-    // }
+    private void SetPosition()
+    {
+        transform.position = new Vector3(Random.Range(-5, 5), 0.8f, Random.Range(7, 15));
+    }
 
     private void Movement()
     {
