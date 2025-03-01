4a4f5eac65621d40619395cfc736e92b301102a1
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-03-01 01:24:30 -0800

    f

--- GDIM_161_Horror/Assets/_Scripts/Player/Starter/FirstPersonController.cs
+++ GDIM_161_Horror/Assets/_Scripts/Player/Starter/FirstPersonController.cs
@@ -91,8 +91,8 @@ namespace StarterAssets
             //_jumpTimeoutDelta = jumpTimeout;
             //_fallTimeoutDelta = fallTimeout;
 
-            //if (_editMode) return;
-            //PlayerModel.SetActive(false);
+            if (_editMode) return;
+            PlayerModel.SetActive(false);
         }
         /*
         private void OnDestroy()
