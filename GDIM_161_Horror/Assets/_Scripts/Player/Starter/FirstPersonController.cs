8644a093e81367ff3ecd53524dbd07a1b5cc5e7f
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-03-01 00:56:37 -0800

    l

--- GDIM_161_Horror/Assets/_Scripts/Player/Starter/FirstPersonController.cs
+++ GDIM_161_Horror/Assets/_Scripts/Player/Starter/FirstPersonController.cs
@@ -82,7 +82,7 @@ namespace StarterAssets
             //_input = GetComponent<StarterAssetsInputs>();
 
             #if ENABLE_INPUT_SYSTEM
-                //_playerInput = GetComponent<PlayerInput>();
+                _playerInput = GetComponent<PlayerInput>();
             #else
                 Debug.LogError("Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
             #endif
