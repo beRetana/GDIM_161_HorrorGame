c3249319001e0ce8b5a9b756cb752ce94c82953e
Brandon Eduardo Retana García <beretana@uci.edu>
2025-03-09 15:47:42 -0700

    Revert "d"
    This reverts commit 2cfc82aeea5892f951ffbab8940ba6e0de957951.

--- GDIM_161_Horror/Assets/_Scripts/Player/Starter/StarterAssetsInputs.cs
+++ GDIM_161_Horror/Assets/_Scripts/Player/Starter/StarterAssetsInputs.cs
@@ -1,15 +1,13 @@
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Mirror;
-
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
 namespace StarterAssets
 {
-	public class StarterAssetsInputs : NetworkBehaviour
+	public class StarterAssetsInputs : MonoBehaviour
 	{
 		[Header("Character Input Values")]
 		public Vector2 move;
@@ -26,12 +24,7 @@ namespace StarterAssets
 		public bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM
-
-        private void Start()
-        {
-            if (!isLocalPlayer) this.enabled = false;
-        }
-        public void OnMove(InputValue value)
+		public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
 		}
