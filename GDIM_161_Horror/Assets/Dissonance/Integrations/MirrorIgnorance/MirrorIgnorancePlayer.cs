ecf181a4e0a9cf857ea6a0648e32a5fce9e03feb
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-03-10 21:57:31 -0700

    maybe fix to find DissonanceComms and reconnect

--- GDIM_161_Horror/Assets/Dissonance/Integrations/MirrorIgnorance/MirrorIgnorancePlayer.cs
+++ GDIM_161_Horror/Assets/Dissonance/Integrations/MirrorIgnorance/MirrorIgnorancePlayer.cs
@@ -1,44 +1,26 @@
 using Mirror;
 using UnityEngine;
+using System.Collections;
 
 namespace Dissonance.Integrations.MirrorIgnorance
 {
-    /// <summary>
-    /// When added to the player prefab, allows Dissonance to automatically track
-    /// the location of remote players for positional audio for games using the
-    /// Mirror Networking API.
-    /// </summary>
-    [RequireComponent(typeof (NetworkIdentity))]
+    [RequireComponent(typeof(NetworkIdentity))]
     public class MirrorIgnorancePlayer
         : NetworkBehaviour, IDissonancePlayer
     {
         private static readonly Log Log = Logs.Create(LogCategory.Network, "Mirror Player Component");
 
         private DissonanceComms _comms;
+        private bool _isSceneTransitioning = false;
 
         public bool IsTracking { get; private set; }
 
-        /// <summary>
-        /// The name of the player
-        /// </summary>
-        /// <remarks>
-        /// This is a syncvar, this means unity will handle setting this value.
-        /// This is important for Join-In-Progress because new clients will join and instantly have the player name correctly set without any effort on our part.
-        /// https://docs.unity3d.com/Manual/UNetStateSync.html
-        /// </remarks>
         [SyncVar]
         private string _playerId;
         public string PlayerId { get { return _playerId; } }
 
-        public Vector3 Position
-        {
-            get { return transform.position; }
-        }
-
-        public Quaternion Rotation
-        {
-            get { return transform.rotation; }
-        }
+        public Vector3 Position => transform.position;
+        public Quaternion Rotation => transform.rotation;
 
         public NetworkPlayerType Type
         {
@@ -49,7 +31,7 @@ namespace Dissonance.Integrations.MirrorIgnorance
                 return _comms.LocalPlayerName.Equals(_playerId) ? NetworkPlayerType.Local : NetworkPlayerType.Remote;
             }
         }
-        
+
         public void OnDestroy()
         {
             if (_comms != null)
@@ -58,56 +40,67 @@ namespace Dissonance.Integrations.MirrorIgnorance
 
         public void OnEnable()
         {
-            _comms = FindObjectOfType<DissonanceComms>();
+            StartCoroutine(FindDissonanceComms());
+        }
+
+        private IEnumerator FindDissonanceComms()
+        {
+            while (_comms == null)
+            {
+                _comms = FindObjectOfType<DissonanceComms>();
+
+                if (_comms != null)
+                {
+                    Debug.Log("[Dissonance] DissonanceComms found, resuming player tracking.");
+                    if (!string.IsNullOrEmpty(PlayerId))
+                        StartTracking();
+                }
+
+                yield return null; // Wait until the next frame to check again
+            }
         }
 
         public void OnDisable()
         {
-            if (!gameObject.scene.isLoaded) return; // Prevent tracking from stopping during a scene transition
+            if (_isSceneTransitioning) return; // Avoid duplicate stop calls
+            _isSceneTransitioning = true;
+
             if (IsTracking)
+            {
+                Debug.Log("[Dissonance] Stopping tracking due to scene transition.");
                 StopTracking();
+            }
         }
 
         public override void OnStartLocalPlayer()
         {
             base.OnStartLocalPlayer();
 
-            var comms = FindObjectOfType<DissonanceComms>();
-            if (comms == null)
+            if (_comms == null)
+                _comms = FindObjectOfType<DissonanceComms>();
+
+            if (_comms == null)
             {
-                throw Log.CreateUserErrorException(
-                    "cannot find DissonanceComms component in scene",
-                    "not placing a DissonanceComms component on a game object in the scene",
-                    "https://dissonance.readthedocs.io/en/latest/Basics/Quick-Start-MirrorIgnorance/",
-                    "2D90A6C3-5F2B-4859-994C-EBBDDD4A10F4"
-                );
+                Debug.LogError("[Dissonance] Cannot find DissonanceComms component in scene!");
+                return;
             }
 
-            Log.Debug("Tracking `OnStartLocalPlayer` Name={0}", comms.LocalPlayerName);
+            Log.Debug("Tracking `OnStartLocalPlayer` Name={0}", _comms.LocalPlayerName);
 
-            // This method is called on the client which has control authority over this object. This will be the local client of whichever player we are tracking.
-            if (comms.LocalPlayerName != null)
-                SetPlayerName(comms.LocalPlayerName);
+            if (_comms.LocalPlayerName != null)
+                SetPlayerName(_comms.LocalPlayerName);
 
-            //Subscribe to future name changes (this is critical because we may not have run the initial set name yet and this will trigger that initial call)
-            comms.LocalPlayerNameChanged += SetPlayerName;
+            _comms.LocalPlayerNameChanged += SetPlayerName;
         }
 
         private void SetPlayerName(string playerName)
         {
-            //We need the player name to be set on all the clients and then tracking to be started (on each client).
-            //To do this we send a command from this client, informing the server of our name. The server will pass this on to all the clients (with an RPC)
-            // Client -> Server -> Client
-
-            //We need to stop and restart tracking to handle the name change
             if (IsTracking)
                 StopTracking();
 
-            //Perform the actual work
             _playerId = playerName;
             StartTracking();
 
-            //Inform the server the name has changed
             if (isLocalPlayer)
                 CmdSetPlayerName(playerName);
         }
@@ -115,33 +108,22 @@ namespace Dissonance.Integrations.MirrorIgnorance
         public override void OnStartClient()
         {
             base.OnStartClient();
+            _isSceneTransitioning = false; // Reset flag
 
-            //A client is starting. Start tracking if the name has been properly initialised.
             if (!string.IsNullOrEmpty(PlayerId))
                 StartTracking();
         }
 
-        /// <summary>
-        /// Invoking on client will cause it to run on the server
-        /// </summary>
-        /// <param name="playerName"></param>
         [Command]
         private void CmdSetPlayerName(string playerName)
         {
             _playerId = playerName;
-
-            //Now call the RPC to inform clients they need to handle this changed value
             RpcSetPlayerName(playerName);
         }
 
-        /// <summary>
-        /// Invoking on the server will cause it to run on all the clients
-        /// </summary>
-        /// <param name="playerName"></param>
         [ClientRpc]
         private void RpcSetPlayerName(string playerName)
         {
-            //received a message from server (on all clients). If this is not the local player then apply the change
             if (!isLocalPlayer)
                 SetPlayerName(playerName);
         }
