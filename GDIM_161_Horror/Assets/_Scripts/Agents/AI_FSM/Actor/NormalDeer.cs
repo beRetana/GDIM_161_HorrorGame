6d9ce910d5d124064baf2b5ba6a194a9a3d2090a
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-03-15 23:00:50 -0700

    e

--- GDIM_161_Horror/Assets/_Scripts/Agents/AI_FSM/Actor/NormalDeer.cs
+++ GDIM_161_Horror/Assets/_Scripts/Agents/AI_FSM/Actor/NormalDeer.cs
@@ -8,14 +8,12 @@ namespace AI
     public class NormalDeer : NetworkAIActor
     {
         private Wander _wander;
-        private Rigidbody _rigidbody;
         private DeerAnimator _animator;
         private NavMeshAgent _controller;
 
         private void Start()
         {
             _wander = GetComponent<Wander>();
-            _rigidbody = GetComponent<Rigidbody>();
             _animator = GetComponent<DeerAnimator>();
             _controller = GetComponent<NavMeshAgent>();
             StartCoroutine(StartSequence());
@@ -24,7 +22,6 @@ namespace AI
         void Update()
         {
             _animator.SetSpeed(Mathf.Clamp01(_controller.velocity.magnitude/_controller.speed));
-            if (_rigidbody.linearVelocity.y > 0.001f) _animator.OnJump();
         }
 
         private IEnumerator StartSequence()
