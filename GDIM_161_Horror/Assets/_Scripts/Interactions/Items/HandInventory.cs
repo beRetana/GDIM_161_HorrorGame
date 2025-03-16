7c2f794cfdeaf174430818aa2981f7c9761f4b7e
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-03-16 16:23:02 -0700

    maybe now

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/HandInventory.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/HandInventory.cs
@@ -300,8 +300,7 @@ public class HandInventory : NetworkBehaviour
         bool isLeftHandAction = (_inventorySlots.GetDominantIndex() == _LEFT_HAND_ID) ^ (!inventorySlotOfNewItem.IsDominant);
 
         inventorySlotOfNewItem.SetRigidBody(pickableParent.GetComponent<Rigidbody>(), _linearDrag);
-        inventorySlotOfNewItem.ItemTransform = pickableParent;
-        pickableParent.SetParent(isLeftHandAction ? _leftHandSocket : _rightHandSocket);
+        inventorySlotOfNewItem.ItemTransform = isLeftHandAction ? _leftHandSocket : _rightHandSocket;
 
         pickableItem.OrientItemInHand(isLeftHandAction);
     }
