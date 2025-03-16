04060fefa3ca19f154cd0f558725742d157a3da5
malachek <malachykennedy1@gmail.com>
2025-03-16 07:24:20 -0700

    its like there p much

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/FireCollision.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/FireCollision.cs
@@ -6,52 +6,30 @@ namespace Interactions
     public class FireCollision : MonoBehaviour
     {
         [SerializeField, Tooltip("Torch / Hearth")]
-        private Transform maybeFireable;
+        private GameObject maybeFireable;
         private IFireable fireableObject;
 
         private const string FIRE_TAG = "Fire";
-        private const string GROUND_TAG = "Ground";
-
-        private bool isSmotherable;
-        private Torch torch; //only has value if isSmotherable
 
         private void Start()
         {
-            fireableObject = maybeFireable.GetComponent<IFireable>();
+            fireableObject = maybeFireable.gameObject.GetComponent<IFireable>();
             if (fireableObject == null) Destroy(this);
 
-            torch = maybeFireable.GetComponent<Torch>();
-            isSmotherable = torch != null;
-            Debug.Log($"{fireableObject}, {maybeFireable.name}, {isSmotherable} smotherable");
         }
 
         private void OnTriggerEnter(Collider col)
         {
-            Debug.Log("Entered Collision");
-            if (isSmotherable && this.IsLit())
-            {
-                // smother check
-                if (!col.CompareTag(GROUND_TAG)) return;
+            if (!col.CompareTag(FIRE_TAG)) return; //check if other is fire
 
-                torch.SmotherFlame();
-                Debug.Log("sending smother");
-                return; //check if this is already lit
-            }
-            else
-            {
-                // share flame check
+            FireCollision colFire = col.gameObject.GetComponent<FireCollision>();
+            Debug.Log($"COLLIDED FIRE {colFire.gameObject.name}, {this}");
 
-                if (!col.CompareTag(FIRE_TAG)) return; //check if other is fire
+            if (!colFire.IsLit()) return; //check if other fire is lit
 
-                FireCollision colFire = col.gameObject.GetComponent<FireCollision>();
-                Debug.Log($"COLLIDED FIRE {colFire.gameObject.name}, {this}");
-
-                if (!colFire.IsLit()) return; //check if other fire is lit
-
-                fireableObject.LightFlame();
-
-            }
+            fireableObject.LightFlame();
         }
+
 
         private bool IsLit()
         {
--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
@@ -37,8 +37,10 @@ namespace Interactions
         [SerializeField, Tooltip("(1)/(1+n) is starting size for flame when lit. larger n = smaller start")
             , Range(1.1f, 10f)] float flameGrowCurveB = 5f;
 
-
         [SerializeField] Light torchLight;
+
+
+        [SerializeField] LayerMask groundLayers;
 
         public float BurnTimer { get; private set; }
         private float burnVelocity = 1f;
@@ -56,6 +58,9 @@ namespace Interactions
         private float flameSize;
         private float maxLightIntensity;
         private float lightIntensity = -1f;
+
+        private bool isDropping = false;
+        private const float SMOTHER_RADIUS = 0.5f;
 
         public bool Lit { get; private set; }
         public bool IsLit() { return Lit; }
@@ -80,6 +85,7 @@ namespace Interactions
         {
             if (!Lit) return;
             UpdateFlameOrientation();
+            SmotherCheck();
         }
 
         private void FixedUpdate()
@@ -94,15 +100,21 @@ namespace Interactions
         }
         public override void UseItem(int playerId)
         {
-            // extend torch in arm
-            // enable torch collider
-            // check if torch colliding with fire
-            // OR
-            // check if handinventory raycast hitting campfire or player
-
-            // ?light torch
             Debug.Log("Using torch");
             PlayerManager.Instance.GetPlayer(playerId).GetComponent<HandInventory>().GetArms().ToggleHandMoveOutOrIn(null);
+        }
+
+
+        private void SmotherCheck()
+        {
+            if (!Lit || !isDropping) return;
+            if (GroundCheck()) SmotherFlame();
+        }
+
+        private bool GroundCheck()
+        {
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            return Physics.CheckSphere(spherePosition, SMOTHER_RADIUS, groundLayers, QueryTriggerInteraction.Ignore);
         }
 
         #region pyrolysis
@@ -181,6 +193,8 @@ namespace Interactions
             lightIntensity = 0f;
             ScaleFlameScale(0);
             SetVisualLightIntensity(0);
+            isDropping = false;
+            ToggleFlame(false);
         }
 
 
@@ -233,13 +247,6 @@ namespace Interactions
             NetworkDestroyTorch();
         }
 
-        #region flame_spread
-        private void RecieveFlameContact(Collider other)
-        {
-            
-        }
-
-        #endregion flame_spread
 
         #region flame_animations
         private IEnumerator BurnOutFire(float burnOutTime, float flameExpDecayRate, float lightExpDecayRate)
@@ -283,6 +290,12 @@ namespace Interactions
             FlameFullSize();
         }
         #endregion flame_animations
+
+        public override void UnPossessItem()
+        {
+            isDropping = true;
+            base.UnPossessItem();
+        }
 
 
         //public void BurnOutFlame() // via end of wood
