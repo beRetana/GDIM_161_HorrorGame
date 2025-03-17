232b9a440aadef1c59ddf7abea2193fde911cf52
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-03-16 17:40:25 -0700

    Revert "now the object goes to the player"
    This reverts commit 0a3b1daa70b73a0428ffcc3707a28aaaf16b107b.

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/HandInventory.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/HandInventory.cs
@@ -290,6 +290,7 @@ public class HandInventory : NetworkBehaviour
     public bool PickUpItem(PickableItem pickableItem)
     {
         PickUpLogic(_inventorySlots.GainItem(pickableItem));
+        _interactableComponent = null;
         return true;
     }
 
@@ -299,7 +300,6 @@ public class HandInventory : NetworkBehaviour
 
         inventorySlotOfNewItem.SetRigidBody(pickableParent.GetComponent<Rigidbody>(), _linearDrag);
         inventorySlotOfNewItem.ItemTransform = isLeftHandAction ? _leftHandSocket : _rightHandSocket;
-        pickableItem.Parent = inventorySlotOfNewItem.ItemTransform;
 
         //pickableItem.OrientItemInHand(isLeftHandAction ? _leftHandSocket : _rightHandSocket);
     }
