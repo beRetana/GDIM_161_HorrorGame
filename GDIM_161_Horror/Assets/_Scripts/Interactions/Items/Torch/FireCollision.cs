b755577eebe75c25107137440eff510e35180991
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-04-28 01:20:34 -0700

    small clean ups

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/FireCollision.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/FireCollision.cs
@@ -6,6 +6,7 @@ namespace Interactions
     public class FireCollision : MonoBehaviour
     {
         [SerializeField, Tooltip("Torch / Hearth")] private GameObject maybeFireable;
+        [SerializeField, Tooltip("Enable Debuglogs")] private bool _debugger;
         private IFireable fireableObject;
 
         private const string FIRE_TAG = "Fire";
@@ -21,17 +22,21 @@ namespace Interactions
             if (!col.CompareTag(FIRE_TAG)) return; //check if other is fire
 
             FireCollision colFire = col.gameObject.GetComponent<FireCollision>();
-            Debug.Log($"COLLIDED FIRE {colFire.gameObject.name}, {this}");
+            Debugger($"COLLIDED FIRE {colFire.gameObject.name}, {this}");
 
             if (!colFire.IsLit()) return; //check if other fire is lit
 
             fireableObject.LightFlame();
         }
 
-
         private bool IsLit()
         {
             return fireableObject.IsLit();
+        }
+
+        private void Debugger(string log)
+        {
+            if (_debugger) Debug.Log(log);
         }
     }
 }
