1516b0a5d0c2b08514d84a8127323acc8be69461
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-05-03 23:29:55 -0700

    updating visuals

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/FireCollision.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/FireCollision.cs
@@ -3,7 +3,7 @@ using Mirror;
 
 namespace Interactions
 {
-    public class FireCollision : MonoBehaviour
+    public class FireCollision : NetworkBehaviour
     {
         [SerializeField, Tooltip("Torch / Hearth")] private GameObject maybeFireable;
         [SerializeField, Tooltip("Enable Debuglogs")] private bool _debugger;
@@ -26,7 +26,20 @@ namespace Interactions
 
             if (!colFire.IsLit()) return; //check if other fire is lit
 
+            if (isServer) RpcLightingObject();
+            else CmdLightingObject();
+        }
+
+        [ClientRpc]
+        private void RpcLightingObject()
+        {
             fireableObject.LightFlame();
+        }
+
+        [Command]
+        private void CmdLightingObject()
+        {
+            RpcLightingObject();
         }
 
         private bool IsLit()
