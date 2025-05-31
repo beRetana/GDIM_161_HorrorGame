e77a174b3dfa5bb1fe0a36e2c398e53d9d6d94de
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-05-31 12:44:35 -0700

    in

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/HandInventory.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/HandInventory.cs
@@ -214,30 +214,31 @@ public class HandInventory : NetworkBehaviour
         // If we hit something in the layer.
         if (Physics.Raycast(rayToInteract, out RaycastHit hitInfo, _pickUpRange, _interactableLayer))
         {
-            IInteractable childCanvas = hitInfo.transform.GetComponentInChildren<IInteractable>();
 
-            // If we didn't hit something before.
-            if (_interactableComponent == null || _interactableComponent.Equals(null))
+            IInteractable newInteractable = hitInfo.transform.GetComponentInChildren<IInteractable>();
+
+            // If we didn't hit something in the previous frame.
+            if (_interactableComponent == null && newInteractable != null)
             {
                 // Report it as detected
-                _interactableComponent = childCanvas;
-                _interactableComponent?.Detected(_playerID);
-                _mouse?.InteractionEffect();
+                _interactableComponent = newInteractable;
+                _interactableComponent.Detected(_playerID);
+                _mouse.InteractionEffect();
             } 
-            // If we are hitting a different object than before.
-            else if (childCanvas != _interactableComponent)
+            // If we are hitting a different object than in the previous frame.
+            else if (newInteractable != _interactableComponent)
             {
                 // Stop animation and start the new one
-                _interactableComponent?.StoppedDetecting(_playerID);
-                _interactableComponent = childCanvas;
-                _interactableComponent?.Detected(_playerID);
+                _interactableComponent.StoppedDetecting(_playerID);
+                _interactableComponent = newInteractable;
+                _interactableComponent.Detected(_playerID);
             }
         }
         // If we didn't hit anything did we hit something before?
-        else if (_interactableComponent != null && !_interactableComponent.Equals(null))
+        else if (_interactableComponent != null)
         {
             _interactableComponent.StoppedDetecting(_playerID);
-            _mouse?.DefaultEffect();
+            _mouse.DefaultEffect();
             _interactableComponent = null;
         }
     }
@@ -252,8 +253,10 @@ public class HandInventory : NetworkBehaviour
     {
         if (_inventorySlots[_LEFT_HAND_ID].Item == null || _inventorySlots[_RIGHT_HAND_ID].Item == null)
         {
-            if (_interactableComponent is PolyInteractable)
-                PolyInteractableSync();
+            _interactableComponent.StoppedDetecting(_playerID);
+            _mouse.DefaultEffect();
+
+            if (_interactableComponent is PolyInteractable) PolyInteractableSync();
             else InteractableSync();
         }
     }
@@ -303,18 +306,32 @@ public class HandInventory : NetworkBehaviour
     private void InteractableSync()
     {
         Debugger($"Interact-Is Player {_playerID} Server: {isServer}");
-        if (isServer) _interactableComponent?.Interact(_playerID);
-        else CmdOnInteract(_interactableComponent?.GetNetworkID(), _playerID);
-        _interactableComponent = null;
+        try
+        {
+            if (isServer) _interactableComponent.Interact(_playerID);
+            else CmdOnInteract(_interactableComponent.GetNetworkID(), _playerID);
+            _interactableComponent = null;
+        }
+        catch 
+        {
+            Debug.LogWarning($"Player {_playerID}: Interactable component is set null");
+        }
     }
 
     private void PolyInteractableSync()
     {
         Debugger($"Interact-Is Player {_playerID} Server: {isServer}");
-        if (isServer) _interactableComponent?.Interact(_playerID);
-        else CmdOnPolyInteract(_interactableComponent?.GetNetworkID(), _playerID, 
-            (_interactableComponent as PolyInteractable).Order);
-        _interactableComponent = null;
+        try
+        {
+            if (isServer) _interactableComponent.Interact(_playerID);
+            else CmdOnPolyInteract(_interactableComponent.GetNetworkID(), _playerID,
+                (_interactableComponent as PolyInteractable).Order);
+            _interactableComponent = null;
+        }
+        catch
+        {
+            Debug.LogWarning($"Player {_playerID}: Interactable component is set null");
+        }
     }
 
     [ClientRpc]
