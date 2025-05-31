b1b97db736798995939d77d1c6c4acc27f670de4
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-05-31 10:28:05 -0700

    testing

--- GDIM_161_Horror/Assets/_Scripts/UI/UI_Testing.cs
+++ GDIM_161_Horror/Assets/_Scripts/UI/UI_Testing.cs
@@ -14,7 +14,7 @@ public class UI_Testing : MonoBehaviour
 
     public void OpenOverlay()
     {
-        if (!SteamAPI.IsSteamRunning())
+        if (SteamAPI.IsSteamRunning())
         {
             SteamFriends.ActivateGameOverlay("Friends"); // Opens Steam Overlay
         }
