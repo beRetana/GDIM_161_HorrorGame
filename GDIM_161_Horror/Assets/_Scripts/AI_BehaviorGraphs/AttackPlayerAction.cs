b60d0cce607288c33c2d23aeff4912b822f531b2
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-06-01 19:15:41 -0700

    enemy

--- GDIM_161_Horror/Assets/_Scripts/AI_BehaviorGraphs/AttackPlayerAction.cs
+++ GDIM_161_Horror/Assets/_Scripts/AI_BehaviorGraphs/AttackPlayerAction.cs
@@ -6,14 +6,17 @@ using Unity.Properties;
 using StarterAssets;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "AttackPlayer", story: "Enemy attacks [Player]", category: "Action", id: "e6b4af2c023fdb12db722fb12e2e7192")]
+[NodeDescription(name: "AttackPlayer", story: "[Enemy] attacks [Player]", category: "Action", id: "e6b4af2c023fdb12db722fb12e2e7192")]
 public partial class AttackPlayerAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Player;
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
 
     protected override Status OnStart()
     {
         FirstPersonController firstPersonController = Player.Value.GetComponent<FirstPersonController>();
+        MonsterAnimator monsterAnimator = Self.Value.GetComponent<MonsterAnimator>();
+        monsterAnimator.TriggerJump();
         firstPersonController.DownPlayer();
         firstPersonController.SetInputState(false);
         return Status.Success;
