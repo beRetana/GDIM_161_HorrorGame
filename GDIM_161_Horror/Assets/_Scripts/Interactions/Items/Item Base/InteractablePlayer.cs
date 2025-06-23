fbe273edcff989857af89b956945ba4f7458fb44
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-06-23 00:40:21 -0700

    it should work

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Item Base/InteractablePlayer.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Item Base/InteractablePlayer.cs
@@ -54,6 +54,7 @@ public class InteractablePlayer : InteractableItem, IDebugger
 
     public override void Interaction(int playerID, InputAction.CallbackContext context)
     {
+        Debugger($"Player {playerID} is trying to interact");
         if (!_isInteractable) return;
 
         switch (context.phase)
