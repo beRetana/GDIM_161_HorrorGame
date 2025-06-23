7d9c2e2dacb6c84e46b61452f856e9287b0cc0f0
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-06-23 02:07:18 -0700

    tweaks

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Item Base/InteractablePlayer.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Item Base/InteractablePlayer.cs
@@ -88,6 +88,5 @@ public class InteractablePlayer : InteractableItem, IDebugger
     {
         Debugger("Player Succesfully Rescued");
         OnPlayerInteract?.Invoke();
-        ResetAnimations();
     }
 }
