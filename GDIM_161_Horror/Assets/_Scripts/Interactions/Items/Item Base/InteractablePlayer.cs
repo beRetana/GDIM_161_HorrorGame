95268311276a077a9d37af596cac1f2326774ff2
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-06-23 01:09:53 -0700

    WHYYYYYY

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Item Base/InteractablePlayer.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Item Base/InteractablePlayer.cs
@@ -54,7 +54,7 @@ public class InteractablePlayer : InteractableItem, IDebugger
 
     public override void Interaction(int playerID, InputAction.CallbackContext context)
     {
-        Debugger($"Player {playerID} is trying to interact: {_isInteractable}");
+        Debugger($"Player {playerID} is trying to interact: {_isInteractable}, phase: {context.phase}");
         if (!_isInteractable) return;
 
         switch (context.phase)
