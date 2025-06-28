300221dfaea9922d097f709cdbdc7f7e86d0c497
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-06-28 16:41:28 -0700

    lol

--- GDIM_161_Horror/Assets/_Scripts/UI/ChooseLab.cs
+++ GDIM_161_Horror/Assets/_Scripts/UI/ChooseLab.cs
@@ -5,6 +5,8 @@ public class ChooseLab : MonoBehaviour
 {
     [SerializeField] private Button m_SmallGameScene;
     [SerializeField] private Button m_BigGameScene;
+    [SerializeField] private Button m_BuildGameScene;
+    [SerializeField] private string m_BuildName;
     [SerializeField] private string m_SmallName;
     [SerializeField] private string m_BigName;
     [SerializeField] private Color m_selected;
@@ -14,12 +16,14 @@ public class ChooseLab : MonoBehaviour
     {
         m_SmallGameScene.onClick.AddListener(SetSmallScene);
         m_BigGameScene.onClick.AddListener(SetBigScene);
+        m_BuildGameScene.onClick.AddListener(SetBuildScene);
     }
 
     private void SetSmallScene()
     {
         m_SmallGameScene.image.color = m_selected;
         m_BigGameScene.image.color = m_Unselected;
+        m_BuildGameScene.image.color = m_Unselected;
         (NewNetworkManager.singleton as NewNetworkManager).SetGameSceneName(m_SmallName);
     }
 
@@ -27,6 +31,15 @@ public class ChooseLab : MonoBehaviour
     {
         m_BigGameScene.image.color = m_selected;
         m_SmallGameScene.image.color = m_Unselected;
+        m_BuildGameScene.image.color = m_Unselected;
         (NewNetworkManager.singleton as NewNetworkManager).SetGameSceneName(m_BigName);
+    }
+
+    private void SetBuildScene()
+    {
+        m_BigGameScene.image.color = m_Unselected;
+        m_SmallGameScene.image.color = m_Unselected;
+        m_BuildGameScene.image.color = m_selected;
+        (NewNetworkManager.singleton as NewNetworkManager).SetGameSceneName(m_BuildName);
     }
 }
