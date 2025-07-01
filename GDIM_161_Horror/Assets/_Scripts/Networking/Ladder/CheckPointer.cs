9b34d2e1eea47dd248b2f211b62d243c449baaae
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-07-01 02:42:32 -0700

    yesyjhsd

--- GDIM_161_Horror/Assets/_Scripts/Networking/Ladder/CheckPointer.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/Ladder/CheckPointer.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class CheckPointer : MonoBehaviour
 {
     [SerializeField] private Transform m_CheckPointPosition;
-    [SerializeField] private ElevatorManager m_LadderManager;
+    [SerializeField] private LadderManager m_LadderManager;
 
-    private void OnTriggerEntered(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<PlayerDataTracker>().SavedPosition = m_CheckPointPosition.position;
         m_LadderManager?.PlayerCheckedIn(other.transform.root.position);
--- GDIM_161_Horror/Assets/_Scripts/Networking/Ladder/LadderManager.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/Ladder/LadderManager.cs
@@ -3,7 +3,7 @@ using OtherUtils;
 using System;
 using UnityEngine;
 
-public class ElevatorManager : NetworkBehaviour, IDebugger
+public class LadderManager : NetworkBehaviour, IDebugger
 {
     [SerializeField] private Transform[] m_Ladders;
     [SerializeField, Range(0f, 1f), Tooltip("Highest probability of choosing the closest ladder")]
