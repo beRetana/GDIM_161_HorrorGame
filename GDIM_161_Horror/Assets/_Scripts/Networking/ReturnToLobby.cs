8e78f49f3e9813726e4121b5fc970fcb34d98a56
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-07-06 00:49:54 -0700

    wierd bug

--- GDIM_161_Horror/Assets/_Scripts/Networking/ReturnToLobby.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/ReturnToLobby.cs
@@ -9,15 +9,11 @@ public class ReturnToLobby : NetworkBehaviour
     [SerializeField] private Button m_ReturnToLobby;
 
     private PlayerManagerHUD m_PlayerManagerHUD;
-    private HandInventory m_HandInventory;
-    private FirstPersonController m_FirstPersonController;
 
     private void Start()
     {
-        if (!isServer) m_ReturnToLobby.gameObject.SetActive(false);
         m_PlayerManagerHUD = transform.root.GetComponent<PlayerManagerHUD>();
-        m_HandInventory = transform.root.GetComponent<HandInventory>();
-        m_FirstPersonController = transform.root.GetComponent<FirstPersonController>();
+        if (!isServer) m_ReturnToLobby.gameObject.SetActive(false);
     }
 
     private void OnEnable()
