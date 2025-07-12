9637b6efa4df45a2ad1f7428d7ff6938519077e8
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-07-12 15:19:06 -0700

    g

--- GDIM_161_Horror/Assets/_Scripts/Parts/ObjectActivator.cs
+++ GDIM_161_Horror/Assets/_Scripts/Parts/ObjectActivator.cs
@@ -25,7 +25,7 @@ public class ObjectActivator : Activator
     [Server]
     private IEnumerator PopulateServerPool()
     {
-        yield return new WaitForSecondsRealtime(0.1f);
+        yield return new WaitForSecondsRealtime(2f);
         Debugger($"Server: Populating Server Pools");
         for(int i = 0; i < m_SpawnLocations.Length; ++i)
         {
