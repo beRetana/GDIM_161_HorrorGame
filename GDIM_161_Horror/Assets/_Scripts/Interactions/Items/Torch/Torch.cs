32948bf1df2cf17144c03d7559ff7e901b7189a6
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2025-07-23 14:55:39 -0700

    Revert "tewaks"
    This reverts commit 201da74c954031de195357618d075d60fe18e843.

--- GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
+++ GDIM_161_Horror/Assets/_Scripts/Interactions/Items/Torch/Torch.cs
@@ -250,7 +250,6 @@ namespace Interactions
 
         private void NetworkDestroyTorch()
         {
-            if (!isLocalPlayer) return;
             Debugger("Ending Torch");
 
             if (OwnerPlayerID != -1)
