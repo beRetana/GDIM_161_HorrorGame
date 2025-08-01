70e4e6b3c5dd1309e34c17b87c882c1c79f9e5e8
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-08-01 13:17:32 -0700

    one of the most commits of all time( pls be prox chat perma fix)

--- GDIM_161_Horror/Assets/_Scripts/Networking/DissonanceReloader.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/DissonanceReloader.cs
@@ -16,7 +16,7 @@ public class DissonanceReloader : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name == "BUILD_1")
+        if (NewNetworkManager.NewSingleton.IsGameplayScene(scene.name))
         {
             _dissonanceComms = FindFirstObjectByType<DissonanceComms>();
             _dissonanceCommsNetwork = FindFirstObjectByType<Dissonance.Integrations.MirrorIgnorance.MirrorIgnoranceCommsNetwork>();
