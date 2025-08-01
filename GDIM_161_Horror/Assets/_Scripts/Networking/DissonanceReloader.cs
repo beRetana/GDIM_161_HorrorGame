b6eba992cf660d3d914c69ef8ab368d68c7f37ee
jajara1 <146236551+jajara1@users.noreply.github.com>
2025-07-31 23:28:35 -0700

    Big save?

--- GDIM_161_Horror/Assets/_Scripts/Networking/DissonanceReloader.cs
+++ GDIM_161_Horror/Assets/_Scripts/Networking/DissonanceReloader.cs
@@ -16,7 +16,7 @@ public class DissonanceReloader : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (NewNetworkManager.NewSingleton.IsGameplayScene(scene.name)) //|| NewNetworkManager.NewSingleton.GetLobbyScene() == scene.name)
+        if (scene.name == "BUILD_1" || scene.name == "Lab_Final_01" || scene.name == "Lab_Final_02" || scene.name == "Lab_Final_03" || scene.name == "Lab_Final_04" || scene.name == "BUILD_Lobby")
         {
             _dissonanceComms = FindFirstObjectByType<DissonanceComms>();
             _dissonanceCommsNetwork = FindFirstObjectByType<Dissonance.Integrations.MirrorIgnorance.MirrorIgnoranceCommsNetwork>();
